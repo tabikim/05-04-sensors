551d3a39c74a949de99111a76e3f2db965176d07
Tabitha Kim <tabijkim@gmail.com>
2016-05-04 12:20:02 -0700

    end of lecture code

--- app/src/main/java/edu/uw/motiondemo/MotionActivity.java
+++ app/src/main/java/edu/uw/motiondemo/MotionActivity.java
@@ -1,14 +1,25 @@
 package edu.uw.motiondemo;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MotionActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MotionActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "Motion";
 
     private TextView txtX, txtY, txtZ;
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
 
 
     @Override
@@ -21,7 +32,50 @@ public class MotionActivity extends AppCompatActivity {
         txtY = (TextView)findViewById(R.id.txt_y);
         txtZ = (TextView)findViewById(R.id.txt_z);
 
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for(Sensor sensor: sensors) {
+            Log.v(TAG, sensor.toString());
+        }
+
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        if(sensor == null) { // no sensor
+            Log.v(TAG, "No Sensor");
+            finish();
+        }
 
     }
 
+    @Override
+    protected void onResume() {
+
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        sensorManager.unregisterListener(this, sensor);
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // when get a new sensor reading
+        float[] values = event.values;
+
+        txtX.setText("" + Math.toDegrees(values[0]));
+        txtY.setText("" + Math.toDegrees(values[1]));
+        txtZ.setText("" + Math.toDegrees(values[2])); // gravity
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
--- app/src/main/java/edu/uw/motiondemo/TouchActivity.java
+++ app/src/main/java/edu/uw/motiondemo/TouchActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.MotionEventCompat;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 public class TouchActivity extends Activity {
@@ -12,6 +13,8 @@ public class TouchActivity extends Activity {
 
     private DrawingSurfaceView view;
 
+    private GestureDetector detector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,22 +22,79 @@ public class TouchActivity extends Activity {
 
         view = (DrawingSurfaceView)findViewById(R.id.drawingView);
 
+        // gesture occurs
+        detector = new GestureDetector(this, new MyGestureListener());
+
+    }
+
+    // what happens when gesture occurs
+    public class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+
+            return true;
+        }
+
+        // what do we want to do on fling
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+            Log.v(TAG, "Fling!");
+            view.ball.dx = -.03f*velocityX;
+            view.ball.dy = -.03f*velocityY;
+
+            return true; // we handled this
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        // if gestured, return true
+        boolean gestured = detector.onTouchEvent(event);
+
         float x = event.getX();
         float y = event.getY();
+
+        int pointerIndex = MotionEventCompat.getActionIndex(event);
+        int pointerID = MotionEventCompat.getPointerId(event, pointerIndex);
 
         //handle action
         int action = MotionEventCompat.getActionMasked(event);
         switch(action) {
             case MotionEvent.ACTION_DOWN:
                 Log.v(TAG,"Touch!");
-                return true;
 
+                view.addTouch(pointerID, event.getX(pointerIndex), event.getY(pointerIndex));
+
+                return true;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                Log.v(TAG, "Pointer Down! Another Finger!");
+
+                view.addTouch(pointerID, event.getX(pointerIndex), event.getY(pointerIndex));
+
+                return true;
+            case MotionEvent.ACTION_POINTER_UP:
+                Log.v(TAG, "Finger left");
+
+                view.removeTouch(pointerID);
+
+                return true;
+            case MotionEvent.ACTION_UP:
+                Log.v(TAG, "Last finger left");
+
+                view.removeTouch(pointerID);
+                return true;
             case (MotionEvent.ACTION_MOVE) : //move
+
+                int pointerCount = event.getPointerCount();
+                for(int i = 0; i < pointerCount; i++) {
+                    int pId = event.getPointerId(i);
+
+                    view.moveTouch(pId, event.getX(i), event.getY(i));
+                }
+
                 return true;
             default :
                 return super.onTouchEvent(event);
